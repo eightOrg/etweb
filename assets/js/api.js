5b040643eaa2d45a341780c77ad32e91189ff9fd
miaoch <476864279@qq.com>
2016-09-10 15:24:39 +0800

    登录测试

--- assets/js/api.js
+++ assets/js/api.js
@@ -8,3 +8,24 @@ var api = {
 function getApiPath(request) {
     return api.server + ":" + api.port + "/" + request ;
 }
+/*
+ajax 模板
+
+$.ajax({
+	type: "GET",
+	url: getApiPath("login"),
+	data: {username:$("#username").val(), password:$("#password").val()},
+	dataType: "json",
+	success: function(data){
+		$('#resText').empty();   //清空resText里面的所有内容
+		var html = '';
+		$.each(data, function(commentIndex, comment){
+			html += '<div class="comment"><h6>' + comment['username']
+				+ ':</h6><p class="para"' + comment['content']
+				+ '</p></div>';
+		});
+		$('#resText').html(html);
+	}
+});
+
+*/
